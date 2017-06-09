35388993860f46506ed7b14353f0958f5b54ceb0
SawyerWelden <sawyer.welden15@ncf.edu>
2017-06-08 20:49:47 -0400

    finished

---
+++ 1/complete.R
@@ -0,0 +1,14 @@
+complete <- function(directory, id = 1:332) {
+  # directory is the directory to find the files in
+  # id is a vecotor of files to analyze, defaulting to all 332
+  # goal is to make a df with the file number 'id' and the num of complete cases 'nobs'
+  files <- list.files(directory)[id]
+  total <- data.frame(id = numeric(), nobs = numeric())
+  for (i in files){
+    f <- read.csv(paste('specdata/',i,sep=""))
+    c <- f[complete.cases(f),]
+    c
+    total[nrow(total)+1,] = c(f[1,]$ID,nrow(c))
+  }
+  total
+}
---
+++ 1/pollutantmean.R
@@ -0,0 +1,13 @@
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+  # directory is the directory to find the files in
+  # pollutant is either 'nitrate' or 'sulfate'
+  # id is a vecotor of files to analyze, defaulting to all 332
+  # goal is to find the mean of the specified pollutant in the specified files
+  files <- list.files(directory)[id]
+  total <- data.frame()
+  for (i in files){
+    f <- read.csv(paste('specdata/',i,sep=""))
+    total <- rbind(total, f)
+  }
+  mean(total[[pollutant]],na.rm=TRUE)
+}
