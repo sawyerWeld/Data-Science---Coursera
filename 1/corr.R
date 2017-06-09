a33426e6d9df9a2363203756b7b3f6fad0e0e77a
SawyerWelden <sawyer.welden15@ncf.edu>
2017-06-09 00:59:22 -0400

    Finished

--- 1/corr.R
+++ 1/corr.R
@@ -2,6 +2,15 @@ corr <- function(directory, threshold=0){
   # directory is the directory to pull files from
   # threshold is lowest count of pairs of observations allowed
   # return 0 if below threshold, else correlation between sulfate and nitrate
-  source("complete.R")
-  complete(directory, 1:2)
+  files <- list.files(directory)
+  corrs <- numeric()
+  for (i in files){
+    f <- read.csv(paste('specdata/',i,sep=""))
+    c <- f[complete.cases(f),]
+    if (nrow(c) >= threshold) {
+      correlation <- cor(c$sulfate,c$nitrate)
+      corrs <- append(corrs,correlation)
+    }
+  }
+  corrs
 }
