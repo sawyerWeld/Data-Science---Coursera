0b18dcefdc3eb114d797b50326347f09a31edac6
SawyerWelden <sawyer.welden15@ncf.edu>
2017-06-11 18:24:04 -0400

    second assignment

---
+++ 2 - Programming in R/Assignment2/cachematrix.R
@@ -0,0 +1,29 @@
+# creates getter and setter methods for the inverse of a matrix
+
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+# returns the inverse of a square matrix, returns from cache if already computed
+
+cacheSolve <- function(x, ...) {
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
+}
