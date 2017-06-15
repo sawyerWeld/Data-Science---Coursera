69f28d0bd3c55081def1d7a588479162e5af56c8
SawyerWelden <sawyer.welden15@ncf.edu>
2017-06-14 22:34:01 -0400

    Question 2

--- 2 - Programming in R/Assignment3/best.R
+++ 2 - Programming in R/Assignment3/best.R
@@ -10,13 +10,16 @@ best <- function(state, outcome) {
   if (!(outcome %in% c("heart attack","heart failure","pneumonia"))){
     stop("invalid outcome")
   }
-  # simplify my data
+  # get only the columns i want to work with and the rows of the state
   simp <- cbind(data[,2],data[,7],data[,11],data[,17],data[,23])
   simp <- data.frame(simp)
   colnames(simp) <- c("name","state.code","attack","failure","pneumonia")
-  simp.state <- subset(simp, state.code == state)
+  simp.state <- droplevels(subset(simp, state.code == state))
+  #coerce data to numbers and return name of min value
   colnum <- if(outcome == "heart attack") {3} else if(outcome == "heart failure") {4} else {5}
-  DATASET$NAME[DATASET$COLUMNNAME == min(DATASET$COLUMNNAME)]
-  # # Return the hospital name of the hospital with the lowest 30-day death rate
-  # 
+  simp.state$name <- as.character(simp.state$name)
+  c <- as.character(simp.state[,colnum])
+  c <- suppressWarnings(as.numeric(c))
+  minrow <- which(c == min(c,na.rm=TRUE))
+  simp.state[minrow,1]
 }
