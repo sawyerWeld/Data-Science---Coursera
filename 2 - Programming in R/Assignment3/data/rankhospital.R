2676ad7aa4dc87fb529dd0797f7cd38de37908f1
SawyerWelden <sawyer.welden15@ncf.edu>
2017-06-14 22:47:12 -0400

    made file

---
+++ 2 - Programming in R/Assignment3/data/rankhospital.R
@@ -0,0 +1,29 @@
+# rankhospital(state, outcome, n) returns the nth best hospital in the state for the specified illness
+
+rankhospital <- function(state, outcome) {
+  # Read data
+  data <- read.csv("outcome-of-care-measures.csv", colClass = "character")
+  # Check that state and outcome are valid
+  if (!(state%in%data$State)){
+    stop("invalid state")
+  }
+  if (!(outcome %in% c("heart attack","heart failure","pneumonia"))){
+    stop("invalid outcome")
+  }
+  # get only the columns i want to work with and the rows of the state
+  simp <- cbind(data[,2],data[,7],data[,11],data[,17],data[,23])
+  simp <- data.frame(simp)
+  colnames(simp) <- c("name","state.code","attack","failure","pneumonia")
+  simp.state <- droplevels(subset(simp, state.code == state))
+  #coerce data to numbers and return name of min value
+  colnum <- if(outcome == "heart attack") {3} else if(outcome == "heart failure") {4} else {5}
+  simp.state$name <- as.character(simp.state$name)
+  c <- as.character(simp.state[,colnum])
+  c <- suppressWarnings(as.numeric(c))
+  
+  
+  
+  
+  minrow <- which(c == min(c,na.rm=TRUE))
+  simp.state[minrow,1]
+}
