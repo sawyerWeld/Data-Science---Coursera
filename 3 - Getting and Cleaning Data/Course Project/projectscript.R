aafdb80fbf75a038a8f2347c23b511d0e9d75ab9
Sawyer <sawyer.welden@gmail.com>
2017-06-29 15:22:14 -0400

    working on course project

--- 3 - Getting and Cleaning Data/Course Project/projectscript.R
+++ 3 - Getting and Cleaning Data/Course Project/projectscript.R
@@ -24,6 +24,42 @@ activities[,2] <- as.character(activities[,2])
 features <- read.table("UCI HAR Dataset/features.txt")
 features[,2] <- as.character(features[,2])
 
+# Get features which contain "mean" or "std"
+
+featuresMeanStd <- grep(".*mean.*|.*std.*",features[,2])
+featuresMeanStd.names <- features[featuresMeanStd,2]
+
+# Make names of features better as recommended by someone on Coursera
+# Things like this are part of tidy data that often I forget about
+# Also the regex in R is wonky coming from Java
+
+featuresMeanStd.names <- gsub('-mean','Mean',featuresMeanStd.names)
+featuresMeanStd.names <- gsub('-std','Std',featuresMeanStd.names)
+featuresMeanStd.names <- gsub('[-()]','',featuresMeanStd.names)
+
+# Load in the data marked as "Training" and "Testing" and bind
+# Seems silly that training and test would be pre-seperated
+# You'd think you'd randomize that and provide the seed
+# But this is why we tidy things up
+# Also rm as we go to conserve RAM
+
+trainingX <- read.table('UCI HAR Dataset/train/X_train.txt')[featuresMeanStd]
+trainingY <- read.table('UCI HAR Dataset/train/y_train.txt')
+trainingSubjects <- read.table('UCI HAR Dataset/train/subject_train.txt')
+train <- cbind(trainingSubjects, trainingY, trainingX)
+rm(trainingX,trainingY,trainingSubjects)
+
+testingX <- read.table('UCI HAR Dataset/test/X_test.txt')[featuresMeanStd]
+testingY <- read.table('UCI HAR Dataset/test/y_test.txt')
+testingSubjects <- read.table('UCI HAR Dataset/test/subject_test.txt')
+test <- cbind(testingSubjects, testingY, testingX)
+rm(testingX,testingY,testingSubjects)
+
+data <- rbind(test,train)
+colnames(data) <- c('subject','activity',featuresMeanStd.names)
+
+rm(test,train)
+
 
 
 
