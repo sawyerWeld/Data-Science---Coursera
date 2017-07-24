17e88b7c0213775682bd056eeb0c4dcefb4169cb
SawyerWelden <sawyer.welden15@ncf.edu>
2017-07-23 20:02:59 -0400

    plot 4

---
+++ 4 - Exploratory Data Analysis/Week 4/plot4.R
@@ -0,0 +1,25 @@
+# Load in data (in plot1.R)
+
+# Require ggplot2
+library(ggplot2)
+
+# Pull SCC's for emissions from coal combustion-related sources
+coal.SCC <- subset(SCC, EI.Sector == "Fuel Comb - Electric Generation - Coal")
+SCCs <- factor(coal.SCC$SCC)
+
+# Subset to only the coal
+coal.data <- subset(NEI, SCC %in% SCCs)
+
+# Aggregate
+coal.by.Year <- aggregate(Emissions ~ year, coal.data, sum)
+
+# Plot
+png('plot4.png')
+ggplot(coal.by.Year, aes(x = year, y = Emissions)) + 
+  geom_line(size = 1.5) + 
+  geom_point(size = 3, fill = "white") +
+  expand_limits(y = 0) +
+  ggtitle("Coal Combustion Emissions in America 1999 - 2008") +
+  ylab(expression('Total PM'[2.5]*'')) +
+  xlab("Year")
+dev.off()
