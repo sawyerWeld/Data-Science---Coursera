9687f66dbff962f35a035b47000ce2b2168a290f
SawyerWelden <sawyer.welden15@ncf.edu>
2017-07-23 20:25:47 -0400

    plot 6

--- 4 - Exploratory Data Analysis/Week 4/plot6.R
+++ 4 - Exploratory Data Analysis/Week 4/plot6.R
@@ -8,15 +8,20 @@ library(ggplot2)
 baltimore.LA.cars <- subset(NEI, fips == "24510" | fips == "06037" & type == 'ON-ROAD')
 
 # Aggregate
-cars.by.Year <- aggregate(Emissions ~ year, baltimore.cars, sum)
+cars.by.Year <- aggregate(Emissions ~ year + fips, baltimore.LA.cars, sum)
+
+# Make data more presentable
+cars.by.Year[cars.by.Year=="06037"] <- "Los Angeles"
+cars.by.Year[cars.by.Year=="24510"] <- "Baltimore"
+colnames(cars.by.Year) <- c("Year", "City", "Emissions")
 
 # Plot
 png('plot6.png')
-ggplot(baltimore.cars.by.Year, aes(x = year, y = Emissions)) + 
+ggplot(cars.by.Year, aes(x = Year, y = Emissions, group = City, colour = City)) + 
   geom_line(size = 1.5) + 
   geom_point(size = 3) +
   expand_limits(y = 0) +
-  ggtitle("Vehicular Emmisions in Baltimore, MD 1999 - 2008") +
-  ylab(expression('Total PM'[2.5]*'')) +
+  ggtitle("Vehicular Emmisions in Baltimore, MD and Los Angeles, Ca 1999 - 2008") +
+  ylab(expression('Total PM'[2.5]*' in tons')) +
   xlab("Year")
 dev.off()
